d44d1611ae04316759593bcc9bc2801c3437bd69
sitoff <svlsitoff@gmail.com>
2022-01-31 12:48:32 +0800

    Опреджеление  моделей

---
+++ Pets_Sheleter/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets_Sheleter.Models
+{
+    class Post
+    {
+        public int Id { get; set; }
+        public string NamePost { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
